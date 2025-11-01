d160520baa916069e479713125f7903c73b755a3
Mahiro <mahiroc36@gmail.com>
2025-11-01 15:19:41 +0300

    added Docker Support

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,27 @@
 import type { NextConfig } from "next";
 
+const isDev = process.env.NODE_ENV === "development";
+
 const nextConfig: NextConfig = {
   async rewrites() {
+    const apiUrl = isDev
+      ? "http://localhost:25401"
+      : "http://backend:25401";
+
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:25401/api/:path*',
+        source: "/api/:path*",
+        destination: `${apiUrl}/api/:path*`,
       },
     ];
   },
   images: {
-    domains: ['images.unsplash.com', "minotar.net", "cdn.modrinth.com", "minecraft-api.vercel.app"],
+    domains: [
+      "images.unsplash.com",
+      "minotar.net",
+      "cdn.modrinth.com",
+      "minecraft-api.vercel.app",
+    ],
   },
 };
 
